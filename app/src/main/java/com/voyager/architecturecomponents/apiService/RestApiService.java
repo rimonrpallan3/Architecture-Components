706446df1753425b7425dcef3124a26fcb2fe339
rimonrpallan3 <rimonrpallan3@gmail.com>
2020-01-06 19:19:36 +0530

    Movie List Changes need to Make

--- app/src/main/java/com/voyager/architecturecomponents/apiService/RestApiService.java
+++ app/src/main/java/com/voyager/architecturecomponents/apiService/RestApiService.java
@@ -3,6 +3,7 @@ package com.voyager.architecturecomponents.apiService;
 import com.voyager.architecturecomponents.activity.ApiTestBarsti.model.TestList;
 import com.voyager.architecturecomponents.activity.ApiTestBarsti.model.TestWrapper;
 import com.voyager.architecturecomponents.activity.Bloger.model.BlogWrapper;
+import com.voyager.architecturecomponents.activity.Bloger.model.MovieLists;
 
 
 import java.util.ArrayList;
@@ -16,6 +17,8 @@ public interface  RestApiService {
 
     @GET("feed.json")
     Call<BlogWrapper> getPopularBlog();
+    @GET("feed.json")
+    Call<MovieLists> getMovieList();
     @GET("locations/10/0")
     Call<ArrayList<TestList>> getTestList();
     @GET("locations/10/0")
